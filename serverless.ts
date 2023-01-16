71f7c77ba045260bd401c1b7e8ff46e3125bb8f6
Ryan Kirsch <agalloch88@gmail.com>
2023-01-16 09:10:36 -0500

    modify serverless.ts to handle bulk functions import

--- serverless.ts
+++ serverless.ts
@@ -1,6 +1,5 @@
 import type { AWS } from '@serverless/typescript';
-
-import hello from '@functions/hello';
+import functions from './serverless/functions'
 
 const serverlessConfiguration: AWS = {
   service: 'urlshortener',
@@ -19,7 +18,7 @@ const serverlessConfiguration: AWS = {
     },
   },
   // import the function via paths
-  functions: { hello },
+  functions,
   package: { individually: true },
   custom: {
     esbuild: {
