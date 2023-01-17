1634553d5f9f9460944f405b2980c7f9a0380aa1
Ryan Kirsch <agalloch88@gmail.com>
2023-01-17 08:29:16 -0500

    finish building dynamoResources

--- serverless.ts
+++ serverless.ts
@@ -1,5 +1,6 @@
 import type { AWS } from '@serverless/typescript';
 import functions from './serverless/functions'
+import dynamoResources from './serverless/dynamoResources';
 
 const serverlessConfiguration: AWS = {
   service: 'urlshortener',
@@ -15,17 +16,20 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+
+      urlTable: '${self:custom.urlTableName}',
     },
   },
   // import the function via paths
   functions,
   resources: {
     Resources: {
-      
+      ...dynamoResources,
     },
   },
   package: { individually: true },
   custom: {
+    urlTableName: '${sls:stage}-url-table',
     esbuild: {
       bundle: true,
       minify: false,
--- serverless/dynamoResources.ts
+++ serverless/dynamoResources.ts
@@ -1,5 +1,25 @@
-import type { AWS } from '@serverless/typescript';
+import type { AWS } from "@serverless/typescript";
 
-const dynamoResources: AWS['resources']['Resources'] = {
-    
+const dynamoResources: AWS["resources"]["Resources"] = {
+  urlTable: {
+    Type: "AWS::DynamoDB::Table",
+    Properties: {
+      TableName: "${self:custom.urlTableName}",
+      AttributeDefinitions: [
+        {
+          AttributeName: "id",
+          AttributeType: "S",
+        },
+      ],
+      KeySchema: [
+        {
+          AttributeName: "id",
+          KeyType: "HASH",
+        },
+      ],
+      BillingMode: "PAY_PER_REQUEST",
+    },
+  },
 };
+
+export default dynamoResources;
