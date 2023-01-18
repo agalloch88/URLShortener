4b7a8f6bd38e74f214c539d60a5c538f3ed467e4
Ryan Kirsch <agalloch88@gmail.com>
2023-01-18 06:53:18 -0500

    add API Gateway file to libs

---
+++ src/libs/apiGateway.ts
@@ -0,0 +1,17 @@
+export const formatJSONResponse = ({
+    statusCode = 200,
+    data = {},
+  }: {
+    statusCode?: number;
+    data?: any;
+  }) => {
+      return {
+          statusCode,
+          body: JSON.stringify(data),
+          headers: {
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Credentials": true,
+          }
+      };
+  };
+  
