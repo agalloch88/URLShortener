775fc8869da3648616323cb80ba61e724382b6f1
Ryan Kirsch <agalloch88@gmail.com>
2023-01-20 16:01:26 -0500

    add getUrl function to serverless functions

--- serverless/functions.ts
+++ serverless/functions.ts
@@ -1,18 +1,28 @@
-import type { AWS } from '@serverless/typescript';
-
+import type { AWS } from "@serverless/typescript";
 
 const functions: AWS["functions"] = {
-    setUrl: {
-        handler: "src/functions/setUrl/index.handler",
-        events: [
-            {
-                httpApi: {
-                    path: "/",
-                    method: "post",
-                }
-            }
-        ]
-    }
+  setUrl: {
+    handler: "src/functions/setUrl/index.handler",
+    events: [
+      {
+        httpApi: {
+          path: "/",
+          method: "post",
+        },
+      },
+    ],
+  },
+  getUrl: {
+    handler: "src/functions/getUrl/index.handler",
+    events: [
+      {
+        httpApi: {
+          path: "/{code}",
+          method: "get",
+        },
+      },
+    ],
+  },
 };
 
 export default functions;
