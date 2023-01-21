efe90c13e280623db8983252de5bace4683f1d36
Ryan Kirsch <agalloch88@gmail.com>
2023-01-21 15:41:23 -0500

    add IAM policy for dynamo

--- serverless.ts
+++ serverless.ts
@@ -11,6 +11,14 @@ const serverlessConfiguration: AWS = {
     runtime: "nodejs14.x",
     profile: "serverlessUser",
     region: "us-east-1",
+    iamRoleStatements: [
+      {
+        Effect: "Allow",
+        Action: "dynamodb:*",
+        Resource:
+          "arn:aws:dynamodb:${self:provider.region}:${aws:accountId}:table/${self:custom.urlTableName}",
+      },
+    ],
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
